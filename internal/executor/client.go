0da038dda831164fc55175ddd137bed0492f1f9e
xiazihao <xiazihao@bytedance.com>
2023-06-20 22:51:58 +0800

    feat: executor client

---
+++ internal/executor/client.go
@@ -0,0 +1,57 @@
+package executor
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	pingEndpoint = "/ping"
+)
+
+type Client struct {
+	c        *http.Client
+	endpoint string
+}
+
+func NewClient(endpoint string) *Client {
+	c := &Client{
+		endpoint: endpoint,
+		c: &http.Client{
+			// Default timeout.
+			Timeout: time.Second * 10,
+		},
+	}
+	return c
+}
+
+// Ping checks the executor is bootstrapped and ready to serve.
+func (c *Client) Ping(ctx context.Context) error {
+	path, err := url.JoinPath(c.endpoint, pingEndpoint)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	request.Header.Add("Content-Type", "application/json")
+	response, err := c.c.Do(request)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer func() {
+		_, _ = io.Copy(io.Discard, response.Body)
+		_ = response.Body.Close()
+	}()
+	if response.StatusCode != http.StatusOK {
+		// Ignore too much payload.
+		body, _ := io.ReadAll(io.LimitReader(response.Body, 1024))
+		return errors.Errorf("invalid status code: %d with payload: %s", response.StatusCode, string(body))
+	}
+	return nil
+}
---
+++ internal/executor/client_test.go
@@ -0,0 +1,42 @@
+package executor
+
+import (
+	"context"
+	internalmodel "github.com/cox96de/runner/internal/model"
+	"github.com/gin-gonic/gin"
+	"gotest.tools/v3/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestClient_Ping(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		engine := gin.New()
+		engine.Any(pingEndpoint, func(c *gin.Context) {
+			c.JSON(http.StatusOK, &internalmodel.Message{Message: "pong"})
+		})
+		server := httptest.NewServer(engine)
+		defer server.Close()
+		client := NewClient(server.URL)
+		err := client.Ping(context.Background())
+		assert.NilError(t, err)
+	})
+	t.Run("bad", func(t *testing.T) {
+		engine := gin.New()
+		message := "not found"
+		engine.Any(pingEndpoint, func(c *gin.Context) {
+			c.JSON(http.StatusNotFound, &internalmodel.Message{Message: message})
+		})
+		server := httptest.NewServer(engine)
+		defer server.Close()
+		client := NewClient(server.URL)
+		err := client.Ping(context.Background())
+		assert.ErrorContains(t, err, message)
+	})
+	t.Run("connection_refuse", func(t *testing.T) {
+		client := NewClient("http://localhost:1234")
+		err := client.Ping(context.Background())
+		assert.ErrorContains(t, err, "connection refused")
+	})
+}
