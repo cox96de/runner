c9244b3576104e5af827c156dd9341fd05c3fa34
xiazihao <xiazihao@bytedance.com>
2023-06-22 17:13:24 +0800

    feat: block ringbuffer if empty or full

---
+++ util/random_test.go
@@ -0,0 +1,13 @@
+package util
+
+import "testing"
+
+func TestRandomInt(t *testing.T) {
+	max := int64(100)
+	for i := 0; i < 1000; i++ {
+		n := RandomInt(max)
+		if n < 0 || n >= max {
+			t.Errorf("RandomInt(%d) = %d, out of range", max, n)
+		}
+	}
+}
