c1c08fabbfe12a4cef3bebd39d349178be0147ab
cox96de <cox96de@gmail.com>
2023-06-29 23:23:09 +0800

    test: add unit test for executing long run command

--- app/executor/handler/command_test.go
+++ app/executor/handler/command_test.go
@@ -5,9 +5,12 @@ import (
 	"context"
 	"io"
 	"os"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/cox96de/runner/util"
 
 	internalmodel "github.com/cox96de/runner/internal/model"
 
@@ -73,7 +76,8 @@ func TestHandler_startCommandHandler(t *testing.T) {
 func TestHandler_getCommandLogHandler(t *testing.T) {
 	testServer, _ := setupHandler(t)
 	client := executor.NewClient(testServer.URL)
-	logs := `go: downloading gotest.tools/v3 v3.4.0
+	t.Run("fast", func(t *testing.T) {
+		logs := `go: downloading gotest.tools/v3 v3.4.0
 go: downloading github.com/google/go-cmp v0.5.5
 ?   	github.com/cox96de/runner/app/executor	[no test files]
 ok  	github.com/cox96de/runner/app/executor/handler	1.053s	coverage: 63.6% of statements in ./...
@@ -81,17 +85,34 @@ ok  	github.com/cox96de/runner/app/executor/handler	1.053s	coverage: 63.6% of st
 ok  	github.com/cox96de/runner/internal/executor	0.028s	coverage: 22.4% of statements in ./...
 ?   	github.com/cox96de/runner/internal/model	[no test files]
 ?   	github.com/cox96de/runner/util	[no test files]`
-	testDir := fs.NewDir(t, "test", fs.WithFile("test.log", logs, fs.WithMode(0o644)))
-	err := client.StartCommand(context.Background(), t.Name(), &executor.StartCommandRequest{
-		Command: []string{"cat", testDir.Join("test.log")},
-		Dir:     os.TempDir(),
+		testDir := fs.NewDir(t, "test", fs.WithFile("test.log", logs, fs.WithMode(0o644)))
+		err := client.StartCommand(context.Background(), t.Name(), &executor.StartCommandRequest{
+			Command: []string{"cat", testDir.Join("test.log")},
+			Dir:     os.TempDir(),
+		})
+		assert.Assert(t, err)
+		l := client.GetCommandLogs(context.Background(), t.Name())
+		buf := &bytes.Buffer{}
+		_, err = io.Copy(buf, l)
+		assert.NilError(t, err)
+		assert.DeepEqual(t, buf.String(), logs)
 	})
-	assert.Assert(t, err)
-	l := client.GetCommandLogs(context.Background(), t.Name())
-	buf := &bytes.Buffer{}
-	_, err = io.Copy(buf, l)
-	assert.NilError(t, err)
-	assert.DeepEqual(t, buf.String(), logs)
+	t.Run("slow", func(t *testing.T) {
+		if runtime.GOOS == "windows" {
+			t.Skip("skip slow test on windows")
+		}
+		commands := []string{"docker run --rm golang:1.20 bash -c \"go env -w GOPROXY=https://goproxy.cn,direct && go install github.com/go-delve/delve/cmd/dlv@latest\""}
+		commandsEnv := util.CompileUnixScript(commands)
+		err := client.StartCommand(context.Background(), t.Name(), &executor.StartCommandRequest{
+			Command: []string{"/bin/sh", "-c", "printf '%s' \"$COMMANDS\" | /bin/sh"},
+			Env:     map[string]string{"COMMANDS": commandsEnv},
+		})
+		assert.Assert(t, err)
+		l := client.GetCommandLogs(context.Background(), t.Name())
+		buf := &bytes.Buffer{}
+		_, err = io.Copy(buf, l)
+		assert.NilError(t, err)
+	})
 }
 
 func TestHandler_getCommandStatusHandler(t *testing.T) {
