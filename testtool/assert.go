e3955f7678ed717c3b2610c73e57e5676c6b8895
cox96de <cox96de@gmail.com>
2023-07-22 12:59:51 +0800

    refactor: assert big object in test tool

--- testtool/assert.go
+++ testtool/assert.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"reflect"
+	"testing"
 
 	"gotest.tools/v3/assert"
 )
@@ -11,28 +12,48 @@ import (
 //go:generate mockgen -destination mock/mockgen_tb.go -package mock . TestingT
 
 type TestingT interface {
-	assert.TestingT
+	Logf(format string, args ...any)
+	FailNow()
 	Helper()
 }
 
 // DeepEqualObject compares the given object with the object in the given file.
 // It uses json as encoder and decoder.
-func DeepEqualObject(t TestingT, got interface{}, expectPath string) {
+func DeepEqualObject(t *testing.T, got interface{}, expectPath string) {
+	t.Helper()
+	AssertObject(t, got, expectPath, func(got, expect interface{}) {
+		assert.DeepEqual(t, got, expect)
+	})
+}
+
+// AssertObject compares the given object with the object in the given file.
+// It uses json as encoder and decoder.
+func AssertObject(t TestingT, got interface{}, expectPath string,
+	assert func(got, expect interface{}),
+) {
 	t.Helper()
 	file, err := os.ReadFile(expectPath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			marshal, err := json.MarshalIndent(got, "", "  ")
-			assert.NilError(t, err)
+			nilError(t, err)
 			err = os.WriteFile(expectPath, marshal, 0o644)
-			assert.NilError(t, err)
+			nilError(t, err)
 		}
-		assert.NilError(t, err)
+		nilError(t, err)
 	}
 	typeOf := reflect.TypeOf(got)
 	v := reflect.New(typeOf)
 	value := v.Interface()
 	err = json.Unmarshal(file, value)
-	assert.NilError(t, err)
-	assert.DeepEqual(t, got, reflect.Indirect(v).Interface())
+	nilError(t, err)
+	assert(got, reflect.Indirect(v).Interface())
+}
+
+func nilError(t TestingT, err error) {
+	if err != nil {
+		t.Helper()
+		t.Logf("error: %v", err)
+		t.FailNow()
+	}
 }
--- testtool/assert_test.go
+++ testtool/assert_test.go
@@ -6,38 +6,51 @@ import (
 
 	"github.com/cox96de/runner/testtool/mock"
 	"go.uber.org/mock/gomock"
+	"gotest.tools/v3/assert"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestDeepEqualObject(t *testing.T) {
+func TestAssertObject(t *testing.T) {
 	t.Run("interface", func(t *testing.T) {
-		DeepEqualObject(t, &corev1.Pod{
+		AssertObject(t, &corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test",
 			},
-		}, "testdata/interface.json")
+		}, "testdata/interface.json", func(got, expect interface{}) {
+			assert.DeepEqual(t, got, expect)
+		})
 	})
 	t.Run("non_interface", func(t *testing.T) {
-		DeepEqualObject(t, corev1.Pod{
+		AssertObject(t, corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test",
 			},
-		}, "testdata/interface.json")
+		}, "testdata/interface.json", func(got, expect interface{}) {
+			assert.DeepEqual(t, got, expect)
+		})
 	})
 	t.Run("not_exists", func(t *testing.T) {
 		expectPath := "testdata/not_exists.json"
 		_ = os.Remove(expectPath)
+		t.Cleanup(func() {
+			_ = os.Remove(expectPath)
+		})
 		controller := gomock.NewController(t)
 		tb := mock.NewMockTestingT(controller)
 		tb.EXPECT().Helper().AnyTimes()
-		tb.EXPECT().Log(gomock.Any()).AnyTimes()
+		tb.EXPECT().Logf(gomock.Any(), gomock.Any()).AnyTimes()
 		tb.EXPECT().FailNow().AnyTimes()
-		DeepEqualObject(tb, corev1.Pod{
+		AssertObject(tb, corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test",
 			},
-		}, expectPath)
+		}, expectPath, func(got, expect interface{}) {
+		})
 	})
+}
+
+func TestDeepEqualObject(t *testing.T) {
+	DeepEqualObject(t, "a", "testdata/deep_equal_object.json")
 }
