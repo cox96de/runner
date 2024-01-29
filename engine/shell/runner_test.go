1f5d09d486be046706341b5ee2b5c589399c73f9
cox96de <cox96de@gmail.com>
2024-01-29 23:59:03 +0800

    fix: block wait for command if command is illegal

--- engine/shell/runner_test.go
+++ engine/shell/runner_test.go
@@ -6,34 +6,38 @@ import (
 	"runtime"
 	"testing"
 
+	"gotest.tools/v3/assert"
+
 	"github.com/cox96de/runner/internal/executor"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestRunner_Start(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		// TODO: support on windows.
-		t.Skipf("windows test is not supported yet")
-	}
 	r := NewRunner()
 	ctx := context.Background()
 	err := r.Start(ctx)
-	assert.NoError(t, err)
+	assert.NilError(t, err)
 	e, err := r.GetExecutor(ctx, "")
-	assert.NoError(t, err)
+	assert.NilError(t, err)
 	err = e.Ping(ctx)
-	assert.NoError(t, err)
-	err = e.StartCommand(ctx, "a", &executor.StartCommandRequest{
-		Dir:     "/tmp",
-		Command: []string{"ls"},
-	})
-	assert.NoError(t, err)
+	assert.NilError(t, err)
+	if runtime.GOOS == "windows" {
+		err = e.StartCommand(ctx, "a", &executor.StartCommandRequest{
+			Dir:     "C:\\",
+			Command: []string{"cmd", "/c", "dir"},
+		})
+	} else {
+		err = e.StartCommand(ctx, "a", &executor.StartCommandRequest{
+			Dir:     "/tmp",
+			Command: []string{"ls"},
+		})
+	}
+	assert.NilError(t, err)
 	logReader := e.GetCommandLogs(ctx, "a")
 	all, err := io.ReadAll(logReader)
-	assert.NoError(t, err)
+	assert.NilError(t, err)
 	t.Logf("%s", string(all))
 	t.Run("stop", func(t *testing.T) {
 		err := r.Stop(ctx)
-		assert.NoError(t, err)
+		assert.NilError(t, err)
 	})
 }
