df354c1ca526cc41f9101dc62edbdeef42487fca
cox96de <cox96de@gmail.com>
2024-10-31 20:43:59 +0800

    ci: use xxci

--- resource/db/gen_test.go
+++ resource/db/gen_test.go
@@ -10,87 +10,91 @@ import (
 	"testing"
 	"time"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+
 	"github.com/cockroachdb/errors"
 
 	"github.com/cenkalti/backoff/v4"
 	"github.com/cox96de/runner/db"
 	"github.com/stretchr/testify/require"
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func TestGenPG(t *testing.T) {
-	name := "runner-gen-ddl-postgres"
-	_, err := run("docker", "run", "--name", name, "--rm", "-e", "POSTGRES_PASSWORD=123456", "-d", "postgres")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		_, _ = run("docker", "stop", name)
+func TestGenDDL(t *testing.T) {
+	if _, err := exec.LookPath("docker"); err != nil {
+		t.Skip("docker not found")
+	}
+	t.Run("GenPG", func(t *testing.T) {
+		name := "runner-gen-ddl-postgres"
+		_, err := run("docker", "run", "--name", name, "--rm", "-e", "POSTGRES_PASSWORD=123456", "-d", "postgres")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			_, _ = run("docker", "stop", name)
+		})
+		ip, err := getContainerIP(name)
+		require.NoError(t, err)
+
+		dsn := fmt.Sprintf("postgresql://postgres:%s@%s:5432/postgres", "123456", ip)
+		var sqls []string
+		err = backoff.Retry(func() error {
+			conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+			if err != nil {
+				return err
+			}
+			dbCli := db.NewClient(db.Postgres, conn)
+			sqls, err = dbCli.ToMigrateSQL()
+			return err
+		}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
+		require.NoError(t, err)
+		t.Logf("%+v", sqls)
+		err = os.WriteFile("ddl.postgres.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
+		require.NoError(t, err)
 	})
-	ip, err := getContainerIP(name)
-	require.NoError(t, err)
-
-	dsn := fmt.Sprintf("postgresql://postgres:%s@%s:5432/postgres", "123456", ip)
-	var sqls []string
-	err = backoff.Retry(func() error {
-		conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		if err != nil {
+	t.Run("GenMySQL", func(t *testing.T) {
+		name := "runner-gen-ddl-mysql"
+		_, err := run("docker", "run", "--name", name, "--rm", "-e", "MYSQL_ROOT_PASSWORD=123456", "-e", "MYSQL_DATABASE=db", "-d", "mysql")
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			_, _ = run("docker", "stop", name)
+		})
+		ip, err := getContainerIP(name)
+		require.NoError(t, err)
+		ip = strings.TrimPrefix(strings.TrimSpace(ip), "'")
+		ip = strings.TrimSuffix(strings.TrimSpace(ip), "'")
+		dsn := fmt.Sprintf("root:%s@tcp(%s:3306)/db", "123456", ip)
+		var sqls []string
+		err = backoff.Retry(func() error {
+			conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+			if err != nil {
+				return err
+			}
+			dbCli := db.NewClient(db.Mysql, conn)
+			sqls, err = dbCli.ToMigrateSQL()
 			return err
-		}
-		dbCli := db.NewClient(db.Postgres, conn)
-		sqls, err = dbCli.ToMigrateSQL()
-		return err
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
-	require.NoError(t, err)
-	t.Logf("%+v", sqls)
-	err = os.WriteFile("ddl.postgres.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
-	require.NoError(t, err)
-}
-
-func TestGenMySQL(t *testing.T) {
-	name := "runner-gen-ddl-mysql"
-	_, err := run("docker", "run", "--name", name, "--rm", "-e", "MYSQL_ROOT_PASSWORD=123456", "-e", "MYSQL_DATABASE=db", "-d", "mysql")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		_, _ = run("docker", "stop", name)
+		}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
+		require.NoError(t, err)
+		t.Logf("%+v", sqls)
+		err = os.WriteFile("ddl.mysql.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
+		require.NoError(t, err)
 	})
-	ip, err := getContainerIP(name)
-	require.NoError(t, err)
-	ip = strings.TrimPrefix(strings.TrimSpace(ip), "'")
-	ip = strings.TrimSuffix(strings.TrimSpace(ip), "'")
-	dsn := fmt.Sprintf("root:%s@tcp(%s:3306)/db", "123456", ip)
-	var sqls []string
-	err = backoff.Retry(func() error {
-		conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err != nil {
+	t.Run("GenSQLite", func(t *testing.T) {
+		var sqls []string
+		err := backoff.Retry(func() error {
+			conn, err := gorm.Open(sqlite.Open("file:runner-gen-ddl-sqlite?mode=memory"), &gorm.Config{})
+			if err != nil {
+				return err
+			}
+			dbCli := db.NewClient(db.Mysql, conn)
+			sqls, err = dbCli.ToMigrateSQL()
 			return err
-		}
-		dbCli := db.NewClient(db.Mysql, conn)
-		sqls, err = dbCli.ToMigrateSQL()
-		return err
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
-	require.NoError(t, err)
-	t.Logf("%+v", sqls)
-	err = os.WriteFile("ddl.mysql.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
-	require.NoError(t, err)
-}
-
-func TestGenSQLite(t *testing.T) {
-	var sqls []string
-	err := backoff.Retry(func() error {
-		conn, err := gorm.Open(sqlite.Open("file:runner-gen-ddl-sqlite?mode=memory"), &gorm.Config{})
-		if err != nil {
-			return err
-		}
-		dbCli := db.NewClient(db.Mysql, conn)
-		sqls, err = dbCli.ToMigrateSQL()
-		return err
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
-	require.NoError(t, err)
-	t.Logf("%+v", sqls)
-	err = os.WriteFile("ddl.sqlite.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
-	require.NoError(t, err)
+		}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 10))
+		require.NoError(t, err)
+		t.Logf("%+v", sqls)
+		err = os.WriteFile("ddl.sqlite.sql", []byte(strings.Join(sqls, ";\n")), 0o644)
+		require.NoError(t, err)
+	})
 }
 
 func getContainerIP(containerName string) (string, error) {
