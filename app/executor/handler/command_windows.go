ced174f1820f595d764fc346be6968ff8f608f24
cox96de <cox96de@gmail.com>
2024-12-26 17:45:10 +0800

    fix: add HOME env in executor

--- app/executor/handler/command_windows.go
+++ app/executor/handler/command_windows.go
@@ -3,9 +3,20 @@ package handler
 import (
 	"os/user"
 	"syscall"
+
+	"github.com/cox96de/runner/log"
 )
 
-func setUserForSysProcAttr(attr *syscall.SysProcAttr, u *user.User) error {
+func init() {
+	var err error
+	realUser, err = user.Current()
+	if err != nil {
+		log.Fatalf("failed to get current user: %v", err)
+	}
+	effectiveUser = realUser
+}
+
+func setUserForSysProcAttr(_ *syscall.SysProcAttr, _ *user.User) error {
 	// Windows doesn't have the concept of setting the user for a process.
 	return nil
 }
